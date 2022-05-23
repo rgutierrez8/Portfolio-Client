39390b8327bdf21bf848dcdc4096ef27537fa9f5
Ramon Gutierrez <ramongutierrez523@gmail.com>
2022-05-23 01:25:06 -0300

    agregado imagenes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  logoAP = "../../../assets/images/APLog.png";
+  myBanner = "../../assets/images/rg.png";
+
   constructor() { }
 
   ngOnInit(): void {
