1b44c75918fbfabf5f01a31ea4d12358859108d1
Ramon Gutierrez <ramongutierrez523@gmail.com>
2022-05-23 11:59:42 -0300

    creado componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ButtonComponent } from './components/header/button/button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SocialNetworksComponent } from './components/header/social-networks/social-networks.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SocialNetworksComponent } from './components/header/social-networks/soc
     PortfolioComponent,
     HeaderComponent,
     ButtonComponent,
-    SocialNetworksComponent
+    SocialNetworksComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
